7cf7bb2ba52fc1b38fc4db63fd7179a7263c8239
Gabilcious <33256895+Gabilcious@users.noreply.github.com>
2017-11-01 19:21:50 +0100

    Update arytmetyka.ml

--- arytmetyka.ml
+++ arytmetyka.ml
@@ -15,11 +15,9 @@ let rec przedzial x y wyn =
 	else (x,-0.)::((0.,y)::wyn)
 
 let min a b = if toNan a then b else if toNan b then a else if a < b then a else b;;
-
 let max a b = if toNan a then b else if toNan b then a else if a > b then a else b;;
 
 let minimum a b c d = min (min a b) (min c d)
-
 let maximum a b c d = max (max a b) (max c d)
 
 let mn x y = 
@@ -28,10 +26,7 @@ let mn x y =
 	|y, x when ((x = infinity) || x = neg_infinity) && (y = 0. || y = -0.) -> 0.
 	|x, y -> x*.y
 
-let dz x y = 
-	match x, y with
-	|x, y -> x/.y
-
+let dz x y = if y = 0 then nan else x /. y 
 
 (***********************
       KONSTRUKTORY      
@@ -76,45 +71,17 @@ let sr_wartosc x = ((min_wartosc x)+.(max_wartosc x))/.2.
          OPERACJE       
 ***********************)
 
-let plus x y =
-	let rec dodaj x y z wyn =
+let operacja x y opr =
+	let rec operuj x y z wyn =
 		match x, y with
 		| _, [] -> wyn
-		| [], _::t -> dodaj z t z wyn
-		| (ax,bx)::tx, (a,b)::_ -> dodaj tx y z (przedzial (ax+.a) (bx+.b) wyn)
+		| [], _::t -> operuj z t z wyn
+		| (ax,bx)::tx, (a,b)::_ -> operuj tx y z (przedzial (opr ax a) (opr bx b) wyn)
 	in match y with
 	| [] -> x
-	| _ -> dodaj x y x []
+	| _ -> operuj x y x []
 
-
-let minus x y =
-	let rec odejmij x y z wyn =
-		match x, y with
-		| _, [] -> wyn
-		| [], _::t -> odejmij z t z wyn
-		| (ax, bx)::tx, (a,b)::_ -> odejmij tx y z (przedzial (ax-.b) (bx-.a) wyn)
-	in match y with
-	| [] -> x
-	| _ -> odejmij x y x []
-
-
-let razy x y =
-	let rec mnoz x y z wyn =
-		match x, y with
-		| _, [] -> wyn
-		| [], _::t -> mnoz z t z wyn
-		| (ax,bx)::tx, (a,b)::_ -> mnoz tx y z (przedzial (minimum (mn ax a) (mn ax b) (mn bx a) (mn bx b)) (maximum (mn ax a) (mn ax b) (mn bx a) (mn bx b)) wyn)
-	in match y with
-	| [] -> x
-	| _ -> mnoz x y x []
-
-
-let podzielic x y =
-	let rec dziel x y z wyn =
-		match x, y with
-		| _, [] -> wyn
-		| [], _::t -> dziel z t z wyn
-		| (ax,bx)::tx, (a,b)::_ -> dziel tx y z (przedzial (minimum (dz ax a) (dz ax b) (dz bx a) (dz bx b)) (maximum (dz ax a) (dz ax b) (dz bx a) (dz bx b)) wyn)
-	in match y with
-	| [] -> x
-	| _ -> dziel x y x []
+let plus x y = operacja x y ( +. )
+let minus x y = operacja x y ( -. )
+let razy x y = operacja x y mn
+let podzielic x y = operacja x y dz
